a759c494c42ca9cca196ef13e07b3a8c4e4e0266
Ali <aizeineddine@gmail.com>
2025-05-26 20:50:37 +0300

    DRF2

--- src/core/settings.py
+++ src/core/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     #django-rest_framework
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 
     #user apps
     'shop.apps.ShopConfig',
@@ -186,5 +188,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #rest framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
