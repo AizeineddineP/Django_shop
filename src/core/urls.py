4dfe39edd158d6788100052e6733e854f3d4044e
Ali <aizeineddine@gmail.com>
2025-05-27 18:43:16 +0300

    added rest_framework_simplejwt

--- src/core/urls.py
+++ src/core/urls.py
@@ -21,6 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,6 +35,9 @@ urlpatterns = [
     # path('hello/first_html', include('shop.urls')),
     # path('home/' , include('shop.urls')),
     # path('marketplace/', include('shop.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
