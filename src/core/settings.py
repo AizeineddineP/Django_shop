597d030f1acd71a858def4bfeb727fabf1eef13f
Ali <aizeineddine@gmail.com>
2025-06-08 19:14:11 +0300

    add tests

--- src/core/settings.py
+++ src/core/settings.py
@@ -115,6 +115,9 @@ DATABASES = {
         "PASSWORD": env("DB_PASSWORD"),
         "HOST": env("DB_HOST"),
         "PORT": env("DB_PORT"),
+        "TEST": {
+            "NAME": "djangoshoptestdatabase",
+        },
     }
 }
 
